ddbf9afe2d9c5864af8617eaf319d338aeacdaac
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-08-10 06:54:04 +0200

    Solved

---
+++ Question-2/Q2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(){
+
+
+    printf("Name: Hilal\n");
+    printf("Grade: A\n");
+
+    getch();    /*to stop cmd from closing after running the .exe*/
+
+    return 0;
+}
