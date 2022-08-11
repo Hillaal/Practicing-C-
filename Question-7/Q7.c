d6d8312be94926adae4c18469041714a2fbf8df7
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-08-11 17:33:01 +0200

    SOLVED

--- Question-7/Q7.c
+++ Question-7/Q7.c
@@ -3,7 +3,7 @@
 
 int main(){
 
-    int num1,num2;
+    int num1,num2,num3,smallest;
 
     printf("Enter First Number \n");
     scanf("%d",&num1);
@@ -11,18 +11,17 @@ int main(){
     printf("Enter Second Number \n");
     scanf("%d",&num2);
 
-    if(num1 == num2){
-        printf("The Two Numbers are Equal\n");
-    }
-    else if(num1 > num2){
-        printf("The Two Numbers are not Equal\n");
-        printf("%d is higher than %d\n",num1,num2);
-    }
-    else{
-        printf("The Two Numbers are not Equal\n");
-        printf("%d is higher than %d\n",num2,num1);
+    printf("Enter Third Number \n");
+    scanf("%d",&num3);
 
-    }
+    smallest = num1;
+
+    if(num2 <= smallest){smallest = num2;}
+    if(num3 <= smallest){smallest = num3;}
+
+    printf("The smallest number is: %d\n",smallest);
+
+
 
     getch();    /*to stop cmd from closing after running the .exe*/
 
