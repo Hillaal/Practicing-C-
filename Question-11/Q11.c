2b99a044e8bc0ba31cf86e039fcfeddb384d9646
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-08-15 16:35:05 +0200

    SOLVED

--- Question-11/Q11.c
+++ Question-11/Q11.c
@@ -1,71 +1,25 @@
 #include <stdio.h>
 
- void calculate(char op,float num1,float num2);
 
 int main(){
 
-    float num1,num2;
-    char operation;
+    int counter,sum = 0;
 
-    printf("Enter Operation from: + or - or * or / \n");
-    scanf("%c",&operation);
+    for(counter = 1; counter <= 100; counter++)
+    {
+        sum = sum + counter;
+    }
 
-    printf("Enter First Number\n");
-    scanf("%f",&num1);
-
-    printf("Enter Second Number\n");
-    scanf("%f",&num2);
-
-    calculate(operation,num1,num2);
+    if(sum == 5050)
+    {
+         printf("sum of the first 100 integers = %d",sum);
+    }
+    else
+    {
+    }
 
     getch();    /*to stop cmd from closing after running the .exe*/
 
     return 0;
 }
-
-
-
-void calculate(char op,float num1,float num2)
-{
-    double result;
-
-    switch(op){
-
-        case '*' :
-        {
-            result = num1 * num2;
-            break;
-        }
-
-        case '/' :
-        {
-            result = num1 / num2;
-            break;
-        }
-
-        case '+':
-        {
-            result = num1 + num2;
-            break;
-        }
-
-        case '-':
-        {
-            result = num1 - num2;
-            break;
-        }
-
-        default:
-            {
-               printf("Enter Valid Operation");
-               return;
-            }
-
-
-    }
-
-    printf("%f %c %f = %lf\n",num1,op,num2,result);
-
-}
-
 
