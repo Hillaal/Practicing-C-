186b0e521928cdba5e17fe35d26a2366aa554e4b
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-08-15 16:28:14 +0200

    q11

---
+++ Question-11/Q11.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+ void calculate(char op,float num1,float num2);
+
+int main(){
+
+    float num1,num2;
+    char operation;
+
+    printf("Enter Operation from: + or - or * or / \n");
+    scanf("%c",&operation);
+
+    printf("Enter First Number\n");
+    scanf("%f",&num1);
+
+    printf("Enter Second Number\n");
+    scanf("%f",&num2);
+
+    calculate(operation,num1,num2);
+
+    getch();    /*to stop cmd from closing after running the .exe*/
+
+    return 0;
+}
+
+
+
+void calculate(char op,float num1,float num2)
+{
+    double result;
+
+    switch(op){
+
+        case '*' :
+        {
+            result = num1 * num2;
+            break;
+        }
+
+        case '/' :
+        {
+            result = num1 / num2;
+            break;
+        }
+
+        case '+':
+        {
+            result = num1 + num2;
+            break;
+        }
+
+        case '-':
+        {
+            result = num1 - num2;
+            break;
+        }
+
+        default:
+            {
+               printf("Enter Valid Operation");
+               return;
+            }
+
+
+    }
+
+    printf("%f %c %f = %lf\n",num1,op,num2,result);
+
+}
+
+
---
+++ Question-11/README.md
@@ -0,0 +1,1 @@
+### Write a program to make a simple calculator usingswitch-case. The calculator takes the operation (+ or – or * or /) and takes the two input arguments and print the results.
