76bef0d73282774b9e0813a7e186bc6ab47bfe9f
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-08-17 19:46:02 +0200

    SOLVED

--- Question-14/Q14.c
+++ Question-14/Q14.c
@@ -1,46 +1,16 @@
 #include <stdio.h>
 
-#define TRUE    (1)
-#define FALSE   (0)
+
 
 
 int main()
 {
+    unsigned char counter;
 
-    long int counter;
-    long int num;
-    unsigned char prime = TRUE;
-
-    do
+    for(counter = 65 ; counter < 91 ; counter ++)
     {
-      printf("Enter Positive Integer\n");
-      scanf("%d",&num);
+            printf("%c\n",counter);
     }
-    while(num < 0);
-
-    for(counter = 2; counter <= (long int)(num/2); counter++)
-    {
-        if( (num % counter) == 0)
-        {
-            prime = FALSE;
-            break;
-        }
-        else
-        {
-            //prime
-        }
-    }
-
-    if(prime == TRUE)
-    {
-        printf("Number Is Prime\n");
-    }
-    else
-    {
-        printf("Number Is Not Prime\n");
-    }
-
-
 
     getch();    /*to stop cmd from closing after running the .exe*/
 
