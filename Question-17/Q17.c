b56ab3ea3d9faf6a48a87e63c7bcd76ee96fe9be
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-08-18 03:49:39 +0200

    SOLVED

--- Question-17/Q17.c
+++ Question-17/Q17.c
@@ -4,28 +4,27 @@
 
 int main()
 {
-
-    long long result = 1;
     long int num;
-    long int power;
-    long int counter;
+    long int digitCounter = 0;
 
-    printf("Enter number\n");
-    scanf("%d",&num);
+
 
     do
     {
-      printf("Enter Positive Power\n");
-      scanf("%d",&power);
+        printf("Enter positive Decimal number\n");
+        scanf("%d",&num);
     }
     while(num < 0);
 
-    for(counter = 1; counter <= power; counter++)
+    if(0 == num){digitCounter = 1;}
+
+    while(num)
     {
-        result = result * num;
+        digitCounter = digitCounter + 1;
+        num = (long int) (num / 10);
     }
 
-    printf("result = %d",result);
+    printf("\nNumber of Digits is: %d\n",digitCounter);
 
     getch();    /*to stop cmd from closing after running the .exe*/
 
