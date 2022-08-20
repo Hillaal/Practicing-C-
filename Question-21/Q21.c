1eac1e8e3deede6072bae37781ae06f701210ee4
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-08-20 20:09:57 +0200

    SOLVED

--- Question-21/Q21.c
+++ Question-21/Q21.c
@@ -2,24 +2,39 @@
 
 
 
+
+
 int main()
 {
    unsigned char counterY;
    unsigned char counterX;
+   unsigned char rowsNo;
 
+   do{
+        printf("ENTER NUMBER OF ROWS\n");
+        scanf("%d",&rowsNo);
+        printf("\n");
+   }while(rowsNo <= 0);
 
-    for (counterY = 5; counterY >=1 ; counterY--)
+    for (counterY = 1; counterY <= rowsNo ; counterY++)
     {
-        for(counterX = 1; counterX <=counterY ; counterX++)
+        for(counterX = 1; counterX <=rowsNo ; counterX++)
         {
-            printf("* ");
+            if ( (counterX == counterY ) || (counterX == rowsNo - counterY + 1 ))
+            {
+                printf(" * ");
+
+            }
+            else
+            {
+                printf(" ");
+
+            }
+
         }
 
         printf("\n");
     }
-
-
-
 
 
     getch();    /*to stop cmd from closing after running the .exe*/
