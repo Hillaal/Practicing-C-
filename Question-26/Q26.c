d0cf2b38bbc1be2d0f7208510297cdeead2f2377
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-08-23 23:05:22 +0200

    SOLVED

--- Question-26/Q26.c
+++ Question-26/Q26.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 
- #define ODD    (1)
- #define EVEN   (0)
+#define ODD    (1)
+#define EVEN   (0)
 
-int main(){
+unsigned char CheckNumber(int num);
 
+int main(void){
+    int num;
+    printf("Enter Number\n");
+    scanf("%d",&num);
 
+    if(0 == num)
+    {
+        printf("Neutral Number\n");
+
+    }
+    else if( (num != 0) && (CheckNumber(num) == EVEN) )
+    {
+        printf("Even Number\n");
+    }
+    else if(CheckNumber(num) == ODD)
+    {
+        printf("Odd Number\n");
+    }
 
     getch();    /*to stop cmd from closing after running the .exe*/
 
@@ -13,3 +30,8 @@ int main(){
 }
 
 
+unsigned char CheckNumber(int num)
+{
+
+    return (num % 2);
+}
