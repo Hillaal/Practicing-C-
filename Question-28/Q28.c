69a37411c61d9abe137f6e5fd77af8e5e91653f5
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-08-24 03:58:26 +0200

    q28\29

---
+++ Question-28/Q28.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(void){
+
+
+
+
+    getch();    /*to stop cmd from closing after running the .exe*/
+
+    return 0;
+}
+
+
---
+++ Question-29/README.md
@@ -0,0 +1,1 @@
+###  Write C Function that converts the any letter from lowercase to uppercase.
