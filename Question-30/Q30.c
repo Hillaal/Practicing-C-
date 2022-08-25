4aeb9c3f0a10df37330d746b60a5a218057dc071
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-08-26 01:08:41 +0200

    SOLVED

--- Question-30/Q30.c
+++ Question-30/Q30.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 
-
+void Swap(int* x, int* y);
 
 int main(void){
 
-  
+    int num1,num2;
+
+    printf("Enter First Number\n");
+    scanf("%d",&num1);
+    printf("Enter Second Number\n");
+    scanf("%d",&num2);
+
+    Swap(&num1, &num2);
+    printf("first number = %d\n",num1);
+    printf("second number = %d\n",num2);
+
     getch();    /*to stop cmd from closing after running the .exe*/
 
     return 0;
 }
 
+
+void Swap(int* x, int* y)
+{
+    *x = (*x) + (*y);
+    *y = (*x) - (*y);
+    *x = (*x) - (*y);
+}
