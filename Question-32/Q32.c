5092ae95e50816204cc60c3ce37a8f6c24fb8777
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-08-27 22:43:04 +0200

    SOLVED

--- Question-32/Q32.c
+++ Question-32/Q32.c
@@ -1,10 +1,56 @@
 #include <stdio.h>
 
+#define TRUE    1
+#define FALSE   0
+
+unsigned char CheckPowerOfTwo(long long number);
 
 int main(void){
 
+    long long num;
+    unsigned char checkResult;
 
+    printf("Enter Number\n\n");
+    scanf("%d",&num);
+
+    if(num <= 0)
+    {
+        printf("\n\nNumber Is NOT Power of Two");
+    }
+    else
+    {
+        if(CheckPowerOfTwo(num))
+        {
+            printf("\n\nNumber Is Power of Two");
+        }
+        else
+        {
+            printf("\n\nNumber Is NOT Power of Two");
+        }
+    }
     getch();    /*to stop cmd from closing after running the .exe*/
 
     return 0;
+}
+
+
+unsigned char CheckPowerOfTwo(long long number)
+
+{   unsigned char flag = TRUE;
+
+    while(number != 1)
+    {
+        if((number % 2) == 0)
+        {
+            number = number / 2;
+        }
+        else
+        {
+            flag = FALSE;
+            break;
+        }
+    }
+
+    return flag;
+
 }
