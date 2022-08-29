82dcd93a76c167ba529e48ceb09be48898f139d1
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-08-29 08:40:05 +0200

    SOLVED

--- Question-33/Q33.c
+++ Question-33/Q33.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
 
+unsigned char GetHeaterActiveTime(unsigned int temp);
 
-int main(void){
 
+int main(void)
+{
+    unsigned int array[10] = {0,30,60,90,100,15,45,75,95,101};
+    unsigned char counter;
+
+    for(counter = 0; counter < 10 ; counter++)
+    {
+        printf("Input Temperature = %d Activation Time = %d Minutes \n\n",array[counter],GetHeaterActiveTime(array[counter]));
+    }
 
     getch();    /*to stop cmd from closing after running the .exe*/
 
     return 0;
+}
+
+
+unsigned char GetHeaterActiveTime(unsigned int temp)
+{
+    if( (temp >= 0) && (temp < 30) )
+       {
+           return 7;
+       }
+    else if( (temp >= 30) && (temp < 60))
+        {
+            return 5;
+        }
+    else if( (temp >= 60) && (temp < 90) )
+        {
+            return 3;
+        }
+    else if( (temp >= 90) && (temp <= 100) )
+        {
+            return 1;
+        }
+    else
+    {
+        return 0;
+    }
 }
