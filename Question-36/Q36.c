c05e4230797915afa61aabbd94de09e9e90f577c
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-09-07 01:21:08 +0200

    SOLVED

--- Question-36/Q36.c
+++ Question-36/Q36.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 
+unsigned char Reverse(unsigned char num);
 
 int main(void){
+
+    unsigned char result;
+    unsigned char num;
+
+
+    printf("Enter 8-bits number \n");
+
+    scanf("%d",&num);
+
+    result = Reverse(num);
+
+    printf("Result = %d",result);
 
 
     getch();    /*to stop cmd from closing after running the .exe*/
 
     return 0;
+}
+
+
+
+unsigned char Reverse(unsigned char num)
+{
+    unsigned char counter = 0;
+    unsigned char res=0;
+
+    for(counter = 0; counter <8; counter++)
+    {
+        res = res + ( ((num & ( 0x01 << counter) ) >> counter)  << (7 - counter ));
+    }
+    return res;
 }
