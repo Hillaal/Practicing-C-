852ef6c81af9a4aa7986d6831a2edbe5d7d0d213
Ahmed Khaled Hilal <Ahmeed.khalledd@gmail.com>
2022-09-07 00:54:29 +0200

    SOLVED

--- Question-35/Q35.c
+++ Question-35/Q35.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 
+double Fibonacci(int x);
+
 
 int main(void){
 
+    long result;
+    int num;
+
+    printf("Enter Integer Number \n");
+    scanf("%d",&num);
+
+    result = Fibonacci(num);
+
+    printf("Result = %d \n",result);
 
     getch();    /*to stop cmd from closing after running the .exe*/
 
     return 0;
+}
+
+
+double Fibonacci(int x)
+{
+    if( (x == 0) || (x == 1) )
+    {
+        return x;
+    }
+    else
+    {
+        return (Fibonacci( x - 1 ) + Fibonacci((x - 2)) );
+    }
 }
